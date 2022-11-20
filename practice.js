c7ce021738dd48084ee5f085034196db218b699b
BertrandMuh <sangwabertrand14@gmail.com>
2022-11-20 00:07:51 -0600

    write  loops to print even and odd number, check for age and inventions.

--- practice.js
+++ practice.js
@@ -1,6 +1,6 @@
 
 const insertALine = () => {
-    console.log('=================');
+    console.log('==================================');
 }
 
 //Write a for loop that logs 0-15
@@ -16,9 +16,13 @@ insertALine();
 //Write a for loop that counts from 15-0
 lowerLimit = 0;
 upperLimit = 15;
+let array = [];
 console.log(`Write a for loop that counts ${upperLimit} - ${lowerLimit}`);
 while(upperLimit >= lowerLimit){
-    console.log(upperLimit);
+    array.push(upperLimit);
+    if(upperLimit == lowerLimit){
+        console.log(array);
+    }
     upperLimit--;
 }
 insertALine();
@@ -26,8 +30,8 @@ insertALine();
 //========================================
 // Make variable laps = 0; write a for loop that logs (“that’s another lap!”) and adds 1 to the laps variable. AFTER the loop, log laps.
 var laps = 0;
-let distanceForALapInFeet = 60;
-let totalDistanceInFeet = 1230;
+let distanceForALapInFeet = 10;
+let totalDistanceInFeet = 45;
 let remainFeetToCompleteAFullLap = distanceForALapInFeet - ( totalDistanceInFeet % distanceForALapInFeet);
 console.log(`Make variable laps = 0; write a for loop that logs (“that's another lap!”) and adds 1 to the laps variable. AFTER the loop, log laps.`);
 for(i = 0; i <= totalDistanceInFeet; i++){
@@ -35,19 +39,73 @@ for(i = 0; i <= totalDistanceInFeet; i++){
         console.log(`That's another lap`);
         laps++;
     }
+    else if(i === totalDistanceInFeet && remainFeetToCompleteAFullLap === 0){
+        console.log(`The total number of laps completed is ${laps}.`);
+        insertALine();
+    }
+    else if(i === totalDistanceInFeet && remainFeetToCompleteAFullLap !== 0){
+        console.log(`The total number of laps completed is ${laps}. You had only ${remainFeetToCompleteAFullLap} feet left to complete a total of ${laps + 1}`);
+        insertALine();
+    }
 }
-if (remainFeetToCompleteAFullLap === distanceForALapInFeet){
-    console.log(`The total number of laps completed is ${laps}.`);
-    insertALine();
+
+//===========================================
+//Make variable age. Make a loop from 0-100. In every loop check if i == age, if so log something, else say “age doesn’t match”.
+lowerLimit = 0;
+upperLimit = 100;
+var age = 90;
+console.log(`Make variable age. Make a loop from ${lowerLimit}-${upperLimit}. In every loop check if i == age, if so log something, else say “age doesn't match`);
+for(lowerLimit; lowerLimit <= upperLimit; lowerLimit++){
+    if(lowerLimit == age){
+        console.log(`The age picked is within the range.`);
+    }
+    else{
+        console.log('age does not match.')
+    }
 }
-else{
-    console.log(`The total number of laps completed is ${laps}. You had only ${remainFeetToCompleteAFullLap} feet left to complete a total of ${laps + 1}`);
-    insertALine();
+insertALine();
+
+//============================================
+//Write a loop that logs all even numbers from 0-700.
+lowerLimit = 0;
+upperLimit = 700;
+array = [];
+console.log(`Write a loop that logs all even numbers from ${lowerLimit}-${upperLimit}.`);
+for(lowerLimit; lowerLimit <= upperLimit; lowerLimit++){
+    if(lowerLimit % 2 == 0){
+        array.push(lowerLimit);
+    }
+    if(lowerLimit == upperLimit){
+        console.log(array);
+        insertALine();
+    }
 }
 
+//===========================================
+//Write a loop that logs all odd numbers from 0-700.
+lowerLimit = 0;
+upperLimit = 700;
+array = [];
+console.log(`Write a loop that logs all odd numbers from ${lowerLimit}-${upperLimit}.`);
+for(lowerLimit; lowerLimit <= upperLimit; lowerLimit++){
+    if(lowerLimit % 2 !== 0){
+        array.push(lowerLimit);
+    }
+    if(lowerLimit == upperLimit){
+        console.log(array);
+        insertALine();
+    }
+}
 
+// Write a loop with some “if else” statements. The loop should start at 1900 and go to 1949 and log when things were invented. For example when i = 1902 the code should log “it is 1902 - the teddy bear is invented!”
+array = [['Zeppelin', 1900], ['Teddy Bear', 1902], ['Talking Motion Picture', 1910], ['Bra', 1913], ['Fortune Cookie', 1918], ['Traffic Signal', 1923], ['Radar', 1935], ['Ballpoint Pen', 1938], ['Slinky', 1943]];
 
-
-
-
+for(i = 1900; i <= 1949; i++){
+    array.forEach(invention => {
+        if(i == invention[1]){
+            console.log(`It is ${invention[1]} - the ${invention[0]} is invented`);
+       } 
+    });
+}
+insertALine();
 
